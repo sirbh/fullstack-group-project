a9564118f6098569b57dd2776a1375790a8cf362
saurabh chauhan <sourabh1202@gmail.com>
2023-11-09 14:56:19 +0200

    added db utils tests

--- test/own/dbUtils.test.js
+++ test/own/dbUtils.test.js
@@ -15,100 +15,100 @@ describe("DataBase Utils", () => {
 
   beforeEach(async () => {
     // reset database
-    // await User.deleteMany({});
-    // await User.create(users);
+    await User.deleteMany({});
+    await User.create(users);
 
-    // // set variables
-    // currentUser = await User.findOne({ email: adminUser.email }).exec();
-    // customer = await User.findOne({ email: customerUser.email }).exec();
-    // response = createResponse();
+    // set variables
+    currentUser = await User.findOne({ email: adminUser.email }).exec();
+    customer = await User.findOne({ email: customerUser.email }).exec();
+    response = createResponse();
   });
   describe("findUserById()", () => {
     it("should return a user with given Id", async () => {
-      // const user = await findUserById(currentUser._id);
-      // expect(user).to.not.be.null;
-      // expect(user).to.not.be.undefined;
-      // expect(user).to.be.an("object");
-      // expect(user.email).to.equal(currentUser.email);
-      // expect(user._id.toString()).to.equal(currentUser._id.toString());
-      // expect(user.name).to.equal(currentUser.name);
-      // expect(user.role).to.equal(currentUser.role);
+      const user = await findUserById(currentUser._id);
+      expect(user).to.not.be.null;
+      expect(user).to.not.be.undefined;
+      expect(user).to.be.an("object");
+      expect(user.email).to.equal(currentUser.email);
+      expect(user._id.toString()).to.equal(currentUser._id.toString());
+      expect(user.name).to.equal(currentUser.name);
+      expect(user.role).to.equal(currentUser.role);
     });
     it("should return null if given Id not found", async () => {
-      // const user = await findUserById("randomId");
-      // expect(user).to.be.null;
+      const user = await findUserById("randomId");
+      expect(user).to.be.null;
     });
   });
   describe("saveUser()", () => {
     it("should save the user with default role of customer", async () => {
-      // const testEmail = `test${adminUser.password}@email.com`;
-      // const userData = { ...adminUser, email: testEmail };
-      // const user = await saveUser(userData);
+      const testEmail = `test${adminUser.password}@email.com`;
+      const userData = { ...adminUser, email: testEmail };
+      const user = await saveUser(userData);
 
-      // expect(user).to.not.be.null;
-      // expect(user).to.not.be.undefined;
-      // expect(user).to.be.an("object");
-      // expect(user.role).to.equal("customer");
-      // expect(user.email).to.equal(testEmail);
-      // expect(user.name).to.equal(userData.name);
+      expect(user).to.not.be.null;
+      expect(user).to.not.be.undefined;
+      expect(user).to.be.an("object");
+      expect(user.role).to.equal("customer");
+      expect(user.email).to.equal(testEmail);
+      expect(user.name).to.equal(userData.name);
     });
     it("should return null if user details are missing", async () => {
-      // const user = await saveUser({});
-      // expect(user).to.be.null;
+      const user = await saveUser({});
+      expect(user).to.be.null;
     });
     it("should return null if email is not in correct format", async () => {
-      // const userData = { ...adminUser, email: 'testEmail' };
-      // const user = await saveUser({});
-      // expect(user).to.be.null;
+      const userData = { ...adminUser, email: 'testEmail' };
+      const user = await saveUser({});
+      expect(user).to.be.null;
     });
     it("should return null if password is missing", async () => {
-        // const userData = { ...adminUser, email: 'testEmail',password: '' };
-        // const user = await saveUser({});
-        // expect(user).to.be.null;
+        const userData = { ...adminUser, email: 'testEmail',password: '' };
+        const user = await saveUser({});
+        expect(user).to.be.null;
       });
   });
 
   describe("removeUser()",()=>{
     it("should remove the user with given Id",async()=>{
-      // const user = await removeUser(customer._id);
-      // const foundUser = await User.findById(customer._id).exec();
-      // expect(foundUser).to.be.null;
+      const user = await removeUser(customer._id);
+      const foundUser = await User.findById(customer._id).exec();
+      expect(foundUser).to.be.null;
     });
     it("should return the removed user",async()=>{
-      // const user = await removeUser(customer._id);
-      // expect(user).to.not.be.null;
-      // expect(user).to.not.be.undefined;
-      // expect(user).to.be.an("object");
-      // expect(user.email).to.equal(customer.email);
-      // expect(user._id.toString()).to.equal(customer._id.toString());
-      // expect(user.name).to.equal(customer.name);
-      // expect(user.role).to.equal(customer.role);
+      const user = await removeUser(customer._id);
+      expect(user).to.not.be.null;
+      expect(user).to.not.be.undefined;
+      expect(user).to.be.an("object");
+      expect(user.email).to.equal(customer.email);
+      expect(user._id.toString()).to.equal(customer._id.toString());
+      expect(user.name).to.equal(customer.name);
+      expect(user.role).to.equal(customer.role);
     });
   })
 
   describe("updateUserDetails()",()=>{
     it("should update the user details",async()=>{
-      // customer.name = 'testName';
-      // const updatedUser = await updateUserDetails(customer);
-      // expect(updatedUser).to.not.be.null;
-      // expect(updatedUser).to.not.be.undefined;
-      // expect(updatedUser).to.be.an("object");
-      // expect(updatedUser.email).to.equal(customer.email);
-      // expect(updatedUser._id.toString()).to.equal(customer._id.toString());
-      // expect(updatedUser.name).to.equal('testName');
-      // expect(updatedUser.role).to.equal(customer.role);
+      customer.name = 'testName';
+      const updatedUser = await updateUserDetails(customer);
+      expect(updatedUser).to.not.be.null;
+      expect(updatedUser).to.not.be.undefined;
+      expect(updatedUser).to.be.an("object");
+      expect(updatedUser.email).to.equal(customer.email);
+      expect(updatedUser._id.toString()).to.equal(customer._id.toString());
+      expect(updatedUser.name).to.equal('testName');
+      expect(updatedUser.role).to.equal(customer.role);
     });
     it("should return null if user details are missing",async()=>{
-      // const updatedUser = await updateUserDetails({});
-      // expect(updatedUser).to.be.null;
+      const updatedUser = await updateUserDetails({});
+      expect(updatedUser).to.be.null;
     });
     it("should return null if email is not in correct format",async()=>{
-      // const updatedUser = await updateUserDetails({email:'testEmail'});
-      // expect(updatedUser).to.be.null;
+      const updatedUser = await updateUserDetails({email:'testEmail'});
+      expect(updatedUser).to.be.null;
     });
     it("should return null if password is m",async()=>{
-      // const updatedUser = await updateUserDetails({email:'testEmail',password:''});
-      // expect(updatedUser).to.be.null;
+      const updatedUser = await updateUserDetails({email:'testEmail',password:''});
+      expect(updatedUser).to.be.null;
     });
   })
 });
