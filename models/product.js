34081c85d0525c21568796b939679bb02f96736e
saurabh chauhan <sourabh1202@gmail.com>
2023-11-18 15:58:26 +0200

    added product model

--- models/product.js
+++ models/product.js
@@ -4,21 +4,35 @@ const Schema = mongoose.Schema;
 const productSchema = new Schema({
   name: {
     type: String,
+    required: true,
+    validate: {
+      validator: function (value) {
+        return value.trim().length > 0; // Ensure name is not an empty string
+      },
+      message: 'Name cannot be an empty string',
+    },
+  },
+  price: {
+    type: Number,
+    required: true,
+    validate: {
+      validator: function (value) {
+        return value > 0; // Ensure price is greater than zero
+      },
+      message: 'Price must be greater than zero',
+    },
+  },
+  image: {
+    type: String,
     required: true
   },
   description: {
     type: String,
     required: true
-  },
-  price: {
-    type: Number,
-    required: true
-  },
-  _id: {
-    type: String,
-    required: true
   }
 });
+
+productSchema.set('toJSON', { virtuals: false, versionKey: false });
 
 const Product = mongoose.model('Product', productSchema);
 
