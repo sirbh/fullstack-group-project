c2f2fbd3750237f8235cc86ea08f0c12a4a77bc0
Laura Karjalainen <laura.k.karjalainen@tuni.fi>
2023-11-19 14:48:52 +0200

    Task 11.4 error fix for user.js

--- models/user.js
+++ models/user.js
@@ -91,7 +91,7 @@ const userSchema = new Schema({
 /**
  * Compare supplied password with user's own (hashed) password
  *
- * @param {string} password
+ * @param {string} password - the entered password
  * @returns {Promise<boolean>} promise that resolves to the comparison result
  */
 userSchema.methods.checkPassword = async function(password) {
